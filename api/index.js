f630a3c6bef346a8368d71fbce19651d9274c917
Salik <thesalikkhan@yahoo.com>
2023-01-13 05:15:13 -0800

    visibility changes

--- api/index.js
+++ api/index.js
@@ -361,20 +361,6 @@ app.get("/api/guess10", (req, res) => {
     items: category.items,
   });
 });
-app.get("/api/answer", (req, res) => {
-  const category = req.query.category;
-  const userAnswers = req.query.answers.split(",");
-  const correctAnswers = categories.find((c) => c.name === category).items;
-  let score = 0;
-  userAnswers.forEach((answer) => {
-    if (correctAnswers.includes(answer)) score++;
-  });
-  res.json({
-    score: score,
-    total: correctAnswers.length,
-  });
-});
-
 app.listen(port, () => {
   console.log(`Guess10 API listening at http://localhost:${port}`);
 });
