cba9f488e988a4cf43e99fd9baa12bc518be41d6
Salik <thesalikkhan@yahoo.com>
2023-01-13 01:37:37 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Backend for Guess10
+
+Written in express.js
+
+## To deploy
+
+```bash
+$ git clone https://github.com/thesalikkhan/guess10-backend.git
+$ cd guess10-backend
+$ npm i
+$ npm start
+```
---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+app.use(cors());
+
+let categories;
+fs.readFile("categories.json", "utf8", (err, data) => {
+  if (err) throw err;
+  categories = JSON.parse(data);
+});
+
+app.get("/guess10", (req, res) => {
+  const category = categories[Math.floor(Math.random() * categories.length)];
+  res.json({
+    category: category.name,
+    items: category.items,
+  });
+});
+app.get("/answer", (req, res) => {
+  const category = req.query.category;
+  const userAnswers = req.query.answers.split(",");
+  const correctAnswers = categories.find((c) => c.name === category).items;
+  let score = 0;
+  userAnswers.forEach((answer) => {
+    if (correctAnswers.includes(answer)) score++;
+  });
+  res.json({
+    score: score,
+    total: correctAnswers.length,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Guess10 API listening at http://localhost:${port}`);
+});
